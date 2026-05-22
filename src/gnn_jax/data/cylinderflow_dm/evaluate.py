b81c1a34de6025e8a281878c7e2c551977e57c4f
Rajarshi Dasgupta <rajarshi99@gmail.com>
2026-05-22 09:50:31 +0900

    Switched to while as dict becomes None in the end

--- src/gnn_jax/data/cylinderflow_dm/evaluate.py
+++ src/gnn_jax/data/cylinderflow_dm/evaluate.py
@@ -18,7 +18,8 @@ def evaluate(model, cfg_eval, data_path, meta_path, test_traj_ids=None):
     eval_dir.mkdir(parents=True, exist_ok=True)
     traj_id = 0
     traj_dict_it = threaded_trajectory_iterator(data_path, meta_path, traj_ids=test_traj_ids)
-    for traj_dict in traj_dict_it:
+    traj_dict = next(traj_dict_it)
+    while traj_dict is not None:
         traj = Trajectory(traj_dict, model.edge_feat_dim)
         def step_fn(v_t,_):
             node_in = jnp.concatenate([v_t, traj.node_type_oh], axis=-1)
@@ -32,3 +33,4 @@ def evaluate(model, cfg_eval, data_path, meta_path, test_traj_ids=None):
         jnp.savez(fout_path, pred=v_seq, gt=traj.vel[1:])
         print(f"traj_id {traj_id} | shape {v_seq.shape} | @ {fout_path}")
         traj_id += 1
+        traj_dict = next(traj_dict_it)
