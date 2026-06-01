e935204560c91355412aa8b7d0d86e98542e0a12
Rajarshi Dasgupta <rajarshi99@gmail.com>
2026-06-01 09:49:12 +0900

    Plotting comparisons

--- expt/train_on_small_cyl/plot_comp.py
+++ expt/train_on_small_cyl/plot_comp.py
@@ -10,11 +10,17 @@ out_dir = Path("output")
 base_dir =  Path("/media/user/HDD-UT/rajarshi_data/cylinder_flow_expt/")
 label_fname = "label.yaml"
 train_fname = "train_logs.csv"
-rollout_dirs = {
-        "On Unseen R": "eval_final",
-        "Test Trajectories": "test",
-        "Test Trajectories with Zero Input": "test_zeroE"
-        }
+rollout_dirs = [
+        "eval_final",
+        "eval_final_zeroE",
+        "test",
+        "test_zeroE" ]
+rollout_labels = [
+        "On Unseen R",
+        "On Unseen R with Zero Input",
+        "Test Trajectories",
+        "Test Trajectories with Zero Input"
+        ]
 
 plot_info_list = []
 for run_dir in base_dir.iterdir():
@@ -30,67 +36,17 @@ for run_dir in base_dir.iterdir():
     plot_info["run_dir"] = run_dir
     plot_info_list.append(plot_info)
 
-# Plot loss values
+# Plot loss values normal and smooth
 plot_loss(train_fname, plot_info_list, out_dir / "loss.png")
-
-# Plot loss values smooth using exp moving avg
 plot_loss(train_fname, plot_info_list, out_dir / "loss_smooth.png", alpha=0.98)
 
 # Plot rollout errors for some trajectories
 for traj_id in range(10):
-    plot_rollout_traj(plot_info_list, rollout_dirs["Test Trajectories"], traj_id, out_dir / f"test_traj_{traj_id:04d}")
-    plot_rollout_traj(plot_info_list, rollout_dirs["On Unseen R"], traj_id, out_dir / f"unseenR_traj_{traj_id:04d}")
+    for rollout_dir in rollout_dirs:
+        plot_rollout_traj(plot_info_list, rollout_dir, traj_id, out_dir / f"{rollout_dir}_{traj_id:04d}.png")
 
-# # Get statistics over all rollouts
-# comp_id = 0
-# comp_list = []
-# for data_label,rollout_dir in rollout_dirs.items():
-#     for run1, run2 in combinations(plot_info_list, 2):
-#         label1 = run1["label"]
-#         label2 = run2["label"]
-#
-#         counts = [0]*4
-#         traj_files1 = {f.name for f in (run1["run_dir"] / rollout_dir).glob("traj_*.npz")}
-#         traj_files2 = {f.name for f in (run2["run_dir"] / rollout_dir).glob("traj_*.npz")}
-#         for traj_f in (traj_files1 & traj_files2): # Set intersection
-#             print(f"{comp_id} | {traj_f} | {data_label} | {rollout_dir}")
-#             err1 = get_rollout_err(run1["run_dir"] / rollout_dir / traj_f)
-#             err2 = get_rollout_err(run2["run_dir"] / rollout_dir / traj_f)
-#             diff = err1 < err2
-#             if jnp.all(diff):
-#                 counts[0] += 1
-#             elif not jnp.any(diff):
-#                 counts[1] += 1
-#             elif diff[0]:
-#                 counts[2] += 1
-#             else:
-#                 counts[3] += 1
-#
-#         bin_names = [f"{label1}\nbetter", f"{label2}\nbetter", f"{label1}\nbetter\ninitially", f"{label2}\nbetter\ninitially"]
-#         colors = [run1["color"], run2["color"]] + 2*["black"]
-#         total = sum(counts)
-#         pct_vals = [100*v/total for v in counts]
-#         plt.title(f"Evaluation on: {data_label} ({total} trajectories)")
-#         plt.ylabel("Percentage of trajectories")
-#         plt.bar(bin_names, pct_vals, color=colors)
-#         plt.savefig(out_dir / f"bar_comp{comp_id}.png")
-#         plt.close()
-#         print(comp_id, counts)
-#
-#         comp = {
-#                 "data": data_label,
-#                 "model_a": label1,
-#                 "model_b": label2,
-#                 "counts": {
-#                     "a_better": counts[0],
-#                     "b_better": counts[1],
-#                     "a_to_b": counts[2],
-#                     "b_to_a": counts[3],
-#                     }
-#                 }
-#         comp_list.append(comp)
-#         comp_id += 1
-#
-# with open(out_dir / "comparison.yaml", "w") as f:
-#     yaml.dump(comp_list, f)
-#
+# Get statistics over all rollouts
+for rollout_dir in rollout_dirs:
+    plot_rollout_stats(plot_info_list, rollout_dir, out_dir / f"{rollout_dir}_stats_beg.png", lambda t: (t < 1))
+    plot_rollout_stats(plot_info_list, rollout_dir, out_dir / f"{rollout_dir}_stats_mid.png", lambda t: (2.5 < t) & (t < 3.5))
+    plot_rollout_stats(plot_info_list, rollout_dir, out_dir / f"{rollout_dir}_stats_end.png", lambda t: (t > 5))
---
+++ expt/train_on_small_cyl/scripts/plot_utils.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# From meta.json
+dt_min = 0.01
+
+def ema(y, alpha=0.8):
+    y_smooth = []
+    y_prev = y[0]
+    for val in y:
+        y_prev = alpha*y_prev + (1 - alpha)*val
+        y_smooth.append(y_prev)
+    return np.array(y_smooth)
+
+def get_rollout_t_vs_err(traj_rollout_path):
+    traj = np.load(traj_rollout_path)
+    if "gt" in traj:
+        err = traj["pred"] - traj["gt"]
+    else:
+        err = traj["pred"] # zeroE case
+    if "t_skip" in traj:
+        t_skip = traj["t_skip"]
+    else:
+        t_skip = 1
+    err = np.linalg.norm(err, axis=-1)
+    err = err.mean(axis=-1) # Average over space
+    t = np.arange(1, err.shape[0]+1) * t_skip * dt_min
+    return t, err
+
+def plot_loss(train_fname, plot_info_list, out_fname, alpha=None):
+    for p in plot_info_list:
+        df = pd.read_csv(p["run_dir"] / train_fname)
+        if alpha is None:
+            plt.plot(df["step"], df["loss"], alpha=0.69, color=p["color"], label=p["label"])
+        else:
+            plt.plot(df["step"], ema(df["loss"], alpha=alpha), alpha=0.69, color=p["color"], label=p["label"])
+    plt.legend()
+    plt.grid()
+    plt.yscale("log")
+    plt.xlabel("Step")
+    plt.ylabel("Loss")
+    print(f"Saving @ {out_fname}")
+    plt.savefig(out_fname)
+    plt.close()
+
+def plot_rollout_traj(plot_info_list, rollout_dir, traj_id, out_fname):
+    ls_list = ["-", "--", "-.", ":"]
+    for p in plot_info_list:
+        eval_dir = p["run_dir"] / rollout_dir
+        t_dirs = sorted(eval_dir.glob("dt_*"))
+        for i,t_dir in enumerate(t_dirs):
+            traj_f = eval_dir / t_dir / f"traj_id_{traj_id:04d}.npz"
+            t_skip_label = t_dir.name.split("_")[-1]
+            if not traj_f.exists():
+                print(404, traj_f)
+                continue
+            t, err = get_rollout_t_vs_err(traj_f)
+            label = p["label"]
+            if i > 0:
+                label += f" {t_skip_label}"
+            plt.plot(t, err, color=p["color"], ls=ls_list[i], label=label)
+    plt.legend()
+    plt.grid()
+    plt.yscale("log")
+    plt.xlabel("Time")
+    plt.ylabel("Error")
+    print(f"Saving @ {out_fname}")
+    plt.savefig(out_fname)
+    plt.close()
+
+def plot_rollout_stats(plot_info_list, rollout_dir, out_fname, mask_fn):
+    err_means = {}
+    colors = {}
+
+    for p in plot_info_list:
+        eval_dir = p["run_dir"] / rollout_dir
+        t_dirs = sorted(eval_dir.glob("dt_*"))
+
+        for i,t_dir in enumerate(t_dirs):
+            t_skip_label = t_dir.name.split("_")[-1]
+            label = p["label"]
+            if i > 0:
+                label += f" {t_skip_label}"
+
+            err_means[label] = []
+            colors[label] = p["color"]
+            print(label, t_dir)
+            for traj_f in t_dir.glob("traj_*.npz"):
+                print(label, traj_f)
+                t, err = get_rollout_t_vs_err(traj_f)
+                err_means[label].append(err[mask_fn(t)].mean())
+
+    x_lab = list(err_means.keys())
+    y_val = [err_means[k] for k in x_lab]
+    color = [colors[k] for k in x_lab]
+
+    med = [np.median(y) for y in y_val]
+    sorted_idx = np.argsort(med)
+
+    x_lab_sorted = [x_lab[i] for i in sorted_idx]
+    y_val_sorted = [y_val[i] for i in sorted_idx]
+    color_sorted = [color[i] for i in sorted_idx]
+
+    bp = plt.boxplot(y_val_sorted, patch_artist=True)
+    for patch, c in zip(bp["boxes"], color_sorted):
+        patch.set_facecolor(c)
+
+    plt.xticks(range(1,len(x_lab_sorted)+1), x_lab_sorted, rotation=45)
+    plt.ylabel("Mean rollout error")
+    plt.yscale("log")
+
+    plt.tight_layout()
+    print(f"Saving @ {out_fname}")
+    plt.savefig(out_fname)
+    plt.close()
