e797563ba1b2c40071ecd198c73b98bae43ae1da
Rajarshi Dasgupta <rajarshi99@gmail.com>
2026-07-06 17:24:31 +0900

    Reruns

--- src/gnn_jax/data/cylinderflow_dm/evaluate.py
+++ src/gnn_jax/data/cylinderflow_dm/evaluate.py
@@ -37,49 +37,50 @@ def evaluate(model, cfg_eval, data_path, meta_path, dt_step=None, test_traj_ids=
     eval_dir = eval_dir / f"dt_{t_skip:02d}"
     eval_dir.mkdir(parents=True, exist_ok=True)
 
+    def rollout(v0, node_type_oh, edge_in, senders, receivers, mask, num_steps):
+        if dt_step is None:
+            def step_fn(v_t,_):
+                node_in = jnp.concatenate([v_t, node_type_oh], axis=-1)
+                pred = model.apply(
+                        variables, node_in, edge_in, senders, receivers
+                        )
+                delta_v = model.apply(
+                        variables,
+                        pred,
+                        method=lambda m,x: m.out_data_norm.denormalize(x)
+                        )
+                v_temp = v_t + delta_v
+                v_next = jnp.where(mask[:,None], v_temp, v_t)
+                return v_next, v_next
+        else:
+            def step_fn(v_t,_):
+                node_in = jnp.concatenate([v_t, node_type_oh], axis=-1)
+                pred = model.apply(
+                        variables, dt_phy, node_in, edge_in, senders, receivers
+                        )
+                delta_v = model.apply(
+                        variables,
+                        pred,
+                        method=lambda m,x: m.out_data_norm.denormalize(x)
+                        )
+                v_temp = v_t + delta_v
+                v_next = jnp.where(mask[:,None], v_temp, v_t)
+                return v_next, v_next
+        return jax.lax.scan(step_fn, v0, length=num_steps)[1]
+    # rollout = jax.jit(rollout)
+
     traj_id = 0
     traj_dict_it = threaded_trajectory_iterator(data_path, meta_path, traj_ids=test_traj_ids)
     traj_dict = next(traj_dict_it)
     while traj_dict is not None:
         traj = Trajectory(traj_dict, model.edge_feat_dim)
-
-        if dt_step is None:
-            def step_fn(v_t,_):
-                node_in = jnp.concatenate([v_t, traj.node_type_oh], axis=-1)
-                pred = model.apply(
-                        variables, node_in, traj.edge_in, traj.senders, traj.receivers
-                        )
-                delta_v = model.apply(
-                        variables,
-                        pred,
-                        method=lambda m,x: m.out_data_norm.denormalize(x)
-                        )
-                v_temp = v_t + delta_v
-                v_next = jnp.where(traj.mask[:,None], v_temp, v_t)
-                return v_next, v_next
-        else:
-            def step_fn(v_t,_):
-                node_in = jnp.concatenate([v_t, traj.node_type_oh], axis=-1)
-                pred = model.apply(
-                        variables, dt_phy, node_in, traj.edge_in, traj.senders, traj.receivers
-                        )
-                delta_v = model.apply(
-                        variables,
-                        pred,
-                        method=lambda m,x: m.out_data_norm.denormalize(x)
-                        )
-                v_temp = v_t + delta_v
-                v_next = jnp.where(traj.mask[:,None], v_temp, v_t)
-                return v_next, v_next
-
-
         if zeroE:
             v0 = jnp.zeros_like(traj.vel[0])
         else:
             v0 = traj.vel[0]
-
+        # num_steps = int((traj.T - 1) // t_skip)
         num_steps = (traj.T - 1) // t_skip
-        _, v_seq = jax.lax.scan(step_fn, v0, length=num_steps)
+        v_seq = rollout(v0, traj.node_type_oh, traj.edge_in, traj.senders, traj.receivers, traj.mask, num_steps)
 
         v_gt = traj.vel[t_skip::t_skip]
         fout_path = eval_dir / f"traj_id_{traj_id:04d}.npz"
