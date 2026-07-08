1aa381527ec4194aa318ed09f2e53ab8f44a720f
Rajarshi Dasgupta <rajarshi99@gmail.com>
2026-07-08 16:13:15 +0900

    Automatic stats file if not found

--- expt/car_aero/tt.py
+++ expt/car_aero/tt.py
@@ -1,21 +1,17 @@
 from gnn_jax.data.drivaerml.load_surface import DrivAerIterator
-import jax
 
 import argparse
 import yaml
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config", type=str, help="Path to config.yaml in train mode")
+parser.add_argument("--config", type=str, help="Path to config.yaml")
 args = parser.parse_args()
-
-rng = jax.random.key(42)
 
 with open(args.config, "r") as f:
     cfg = yaml.safe_load(f)
-base_dir = cfg["dataset"]["dir"]
-data_cfg = cfg["dataset"]["cfg"]
 
-drivaer_iter = iter(DrivAerIterator(rng, base_dir, data_cfg))
-mesh, data = next(drivaer_iter)
-print(mesh, data)
+drivaer_iter = DrivAerIterator(cfg["dataset"])
+for io_dict in drivaer_iter:
+    for k,v in io_dict.items():
+        print(k, type(v))
 
--- src/gnn_jax/data/drivaerml/load_surface.py
+++ src/gnn_jax/data/drivaerml/load_surface.py
@@ -31,9 +31,11 @@ def get_io(stl_path, vtp_path):
     k = faces[:, 2]
     senders = np.concatenate([i, j, k, j, k, i])
     receivers = np.concatenate([j, k, i, i, j, k])
+    io_dict["senders"] = senders
+    io_dict["receivers"] = receivers
 
     rel_pos = vertices[receivers] - vertices[senders]
-    distance = np.linalg.norm(rel, axis=-1, keepdims=True)
+    distance = np.linalg.norm(rel_pos, axis=-1, keepdims=True)
     io_dict["edge_in"] = {
             "rel_pos" : rel_pos,
             "distance": distance
@@ -67,7 +69,7 @@ def get_io(stl_path, vtp_path):
 
     io_dict["node_out"] = {
             "Cp": Cp_out,
-            "shear_stree": shear_stress_out
+            "shear_stress": shear_stress_out
             }
 
     return io_dict
@@ -100,25 +102,24 @@ class AccumulateStats:
             glob_avg = np.sum(avg*cnt[:,None], axis=0) / np.sum(cnt)
             glob_var = np.sum(cnt[:,None] * (var + (avg - glob_avg)**2), axis=0) / np.sum(cnt)
             result[name] = {
-                    "avg": glob_avg,
-                    "std": np.sqrt(glob_var)
+                    "avg": glob_avg.tolist(),
+                    "std": np.sqrt(glob_var).tolist()
                     }
 
         return result
 
-
-
 class DrivAerIterator:
-    def __init__(self, rng, cfg_data, mode="train"):
+    def __init__(self, cfg_data, mode="train"):
         self.base_path = Path(cfg_data["dir"])
-        stats_path = Path(cfg_data["stats"])
+        self.stats_path = Path(cfg_data["stats"])
+        self.mode = mode
 
-        run_dirs = np.array([p for p in self.base_path.iterdir() if p.is_dir()])
+        run_dirs = np.array([p.name for p in self.base_path.iterdir() if p.is_dir()])
 
-        if stats_path.exists():
-            with open(stats_path, "r") as f:
+        if self.stats_path.exists():
+            with open(self.stats_path, "r") as f:
                 stats = json.load(f)
-            split = stats["split"]
+            self.split = stats["split"]
             self.max_nodes = stats["max_nodes"]
             self.max_edges = stats["max_edges"]
             self.node_in_stats = stats["node_in"]
@@ -126,8 +127,8 @@ class DrivAerIterator:
             self.node_out_stats = stats["node_out"]
 
         else:
-            print(f"Creating dataset split as {stats_path} not found")
-            perm = jax.random.permutation(rng, len(run_dirs))
+            print(f"Creating dataset split as {self.stats_path} not found")
+            perm = np.random.permutation(len(run_dirs))
             shuffled = run_dirs[perm]
 
             N = len(shuffled)
@@ -147,8 +148,9 @@ class DrivAerIterator:
             node_out = AccumulateStats()
 
             for i,run_dir in enumerate(train_dirs):
+                print(run_dir, type(run_dir))
                 run_path = self.base_path / run_dir
-                stl_path = list(run_path.glob("drivaer_*_single_solid.stl"))[0]
+                stl_path = list(run_path.glob("drivaer_*.stl"))[0]
                 vtp_path = list(run_path.glob("boundary_*.vtp"))[0]
                 try:
                     io_dict = get_io(stl_path, vtp_path)
@@ -159,55 +161,70 @@ class DrivAerIterator:
                     if self.max_nodes < n_node:
                         self.max_nodes = n_node
                     if self.max_edges < n_edge:
-                        self.max_nodes = n_edge
+                        self.max_edges = n_edge
 
                     print(f"Train ID: {i}")
                     good_id.append(i)
                 except:
                     print(f"BAD run_dir {run_dir}")
                     bad_id.append(i)
-                    continue
+
+                if i == 5:
+                    break
             
-            train_dirs = [train_dirs[i] for i in good_id]
             bad_dirs = [train_dirs[i] for i in bad_id]
-            split = {
-                    "train" : train_dirs,
+            new_train_dirs = [train_dirs[i] for i in good_id]
+            self.split = {
+                    "train" : new_train_dirs,
                     "val"   : val_dirs,
                     "test"  : test_dirs,
                     "bad"   : bad_dirs
                     }
-            print(split)
+            print(self.split)
 
             self.node_in_stats = node_in.get_avg_std()
             self.edge_in_stats = edge_in.get_avg_std()
             self.node_out_stats = node_out.get_avg_std()
 
-            stats = {
-                    "split"    : split,
-                    "max_nodes": self.max_nodes,
-                    "max_edges": self.max_edges,
-                    "node_in"  : self.node_in_stats,
-                    "edge_in"  : self.edge_in_stats,
-                    "node_out" : self.node_out_stats
-                    }
+            self.save_stats()
 
-            with open(stats_path, "w") as f:
-                json.dump(stats, f)
+    def save_stats(self):
+        stats = {
+                "split"    : self.split,
+                "max_nodes": self.max_nodes,
+                "max_edges": self.max_edges,
+                "node_in"  : self.node_in_stats,
+                "edge_in"  : self.edge_in_stats,
+                "node_out" : self.node_out_stats
+                }
 
-        self.run_dirs = split[mode]
+        with open(self.stats_path, "w") as f:
+            json.dump(stats, f)
 
     def __len__(self):
-        return len(self.run_dirs)
+        return len(self.split[self.mode])
 
     def __iter__(self):
-        for run_dir in self.run_dirs:
+        bad_id = []
+        good_id = []
+        for i,run_dir in enumerate(self.split[self.mode]):
             print(run_dir)
             run_path = self.base_path / run_dir
-            stl_path = list(run_path.glob("drivaer_*_single_solid.stl"))[0]
+            stl_path = list(run_path.glob("drivaer_*.stl"))[0]
             vtp_path = list(run_path.glob("boundary_*.vtp"))[0]
-            graph, target_node_out = get_io(stl_path, vtp_path)
 
-            # Pad the graph = bla bla
+            try:
+                io_dict = get_io(stl_path, vtp_path)
+                good_id.append(i)
+                yield io_dict
+            except:
+                print(f"BAD run_dir {run_dir}")
+                bad_id.append(i)
 
-            yield graph, target_node_out
+        for i in bad_id:
+            self.split["bad"].append(self.split[self.mode][i])
+        new_run_dir_list = [self.split[self.mode][i] for i in good_id]
+        self.split[self.mode] = new_run_dir_list
+        self.save_stats()
+
 
